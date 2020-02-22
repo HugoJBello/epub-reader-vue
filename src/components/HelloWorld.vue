2eec99c95c42b0ea325a3475ab6d5566c5e76e1a
hugo <hjbello.wk@gmail.com>
2020-02-23 00:15:08 +0100

    fix

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div id="viewer"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Epub from 'epubjs'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  public book :any;
+  public epubUrl = "/public/test.epub";
+  mounted(){
+      this.book = Epub(this.epubUrl) as any;
+      const rendition = this.book.renderTo("viewer");
+      rendition.display();
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
